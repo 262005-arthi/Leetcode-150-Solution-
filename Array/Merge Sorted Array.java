963649d25749401d731b5cb6d9125fd01055b710
262005-arthi <168403510+262005-arthi@users.noreply.github.com>
2025-08-31 18:30:12 +0530

    Update Merge Sorted Array.java

--- Array/Merge Sorted Array.java
+++ Array/Merge Sorted Array.java
@@ -1,1 +1,24 @@
-
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+    	int i,j;
+	j=0;
+	
+	for (i=0; i<m+n; i++){
+		if (i<m){
+		     nums1[i] = nums1[i];
+           	printf("%d\n",nums1[i]);
+		}
+		else{
+			nums1[i] = nums2[j];
+			j++;
+		}
+	}
+	for (i=0; i<nums1Size; i++){
+        for (j=i+1; j<nums1Size; j++){
+		if (nums1[i] > nums1[j]){
+            int temp = nums1[i];
+            nums1[i] = nums1[j];
+            nums1[j] = temp;
+        }
+        }
+	}  
+}
